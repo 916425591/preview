27ec2145a91f10af83ce393cb177d69f925c583b
917425591 <916425591@qq.com>
2022-06-02 01:01:16 +0800

    feat(add) 新增效果切换

--- config.js
+++ config.js
@@ -1,3 +1,21 @@
-window.baseSceneUrl='/dist/models/demo/demo0528.gltf';
+window.baseSceneUrl='/preview/models/demo/demo0528.gltf';
 window.baseUrl='';//服务器目录
-window.baseImage='/dist/image/';//服务器目录
+window.baseImage='/preview/image/';//服务器目录
+/*临时加一个效果切换按钮*/
+const dom =  document.createElement('div');
+let isClick = false;
+dom.style.background = "#092266";
+dom.style.borderRadius="25px";
+dom.style.padding = "6px 10px";
+dom.style.position = "absolute";
+dom.style.right = "0.1%";
+dom.style.bottom = "0.5%";
+dom.innerHTML = "效果切换";
+dom.addEventListener('click',()=>{
+    if(!isClick) {
+        META.tunnel.loadeffect();
+        isClick = true;
+    }
+})
+document.body.appendChild(dom);
+
